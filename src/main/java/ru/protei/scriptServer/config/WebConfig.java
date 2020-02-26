3900f7a1d70dd8c93c44a16635d9b44bbdefba29
MyMindWorld <gromovmx355@gmail.com>
2020-02-27 01:00:18 +0300

    move resources, update config

--- src/main/java/ru/protei/scriptServer/config/WebConfig.java
+++ src/main/java/ru/protei/scriptServer/config/WebConfig.java
@@ -22,32 +22,9 @@ public class WebConfig implements WebMvcConfigurer {
         resolver.setViewClass(JstlView.class);
         registry.viewResolver(resolver);
     }
+
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-        // Register resource handler for CSS
-        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/css/")
-                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
-
-        // Register resource handler for JS
-        registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/js/")
-                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
-
-        // Register resource handler for images
-        registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/images/")
-                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
-
-        // Register resource handler for fonts
-        registry.addResourceHandler("/fonts/**").addResourceLocations("/WEB-INF/fonts/")
-                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
-
-        // Register resource handler for vendor
-        registry.addResourceHandler("/vendor/**").addResourceLocations("/WEB-INF/vendor/")
-                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
     }
 }

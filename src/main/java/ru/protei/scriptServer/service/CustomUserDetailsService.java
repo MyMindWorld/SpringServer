42847368ccaf8c8dc0c67db0ac067da439ad4680
MyMindWorld <gromovmx355@gmail.com>
2020-03-07 18:24:18 +0300

    Fix email acting like username)

--- src/main/java/ru/protei/scriptServer/service/CustomUserDetailsService.java
+++ src/main/java/ru/protei/scriptServer/service/CustomUserDetailsService.java
@@ -48,7 +48,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         }
 
         return new org.springframework.security.core.userdetails.User(
-                user.getEmail(), user.getPassword(), user.isEnabled(), true, true,
+                user.getUsername(), user.getPassword(), user.isEnabled(), true, true,
                 true, getAuthorities(user.getRoles()));
     }
 
